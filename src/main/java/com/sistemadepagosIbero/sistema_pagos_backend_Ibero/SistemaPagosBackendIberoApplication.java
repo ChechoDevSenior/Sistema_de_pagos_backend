06cd43d12e025a6f1f92542e0ffac7713e8390a2
Sergio Arturo Peña Chaparro <sergioarturo9306@gmail.com>
2025-05-04 19:11:19 -0500

    Primer_commit

---
+++ src/main/java/com/sistemadepagosIbero/sistema_pagos_backend_Ibero/SistemaPagosBackendIberoApplication.java
@@ -0,0 +1,13 @@
+package com.sistemadepagosIbero.sistema_pagos_backend_Ibero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagosBackendIberoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagosBackendIberoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemadepagosIbero/sistema_pagos_backend_Ibero/SistemaPagosBackendIberoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemadepagosIbero.sistema_pagos_backend_Ibero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagosBackendIberoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
